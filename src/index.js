e1b7baf5ea962cc8afc7bc81070729f04a577b62
Sofiya Polienko <sofie.polienko@gmail.com>
2021-02-23 15:14:57 +0500

    feat: 8/8 tests passing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+function oddSort(a, b) {
+  return a - b;
 }
+
+function evenSort(a, b) {
+  return b - a;
+}
+
+module.exports = function towelSort (matrix) {
+	if (!matrix || matrix.length === 0) 
+		return [];
+for ( let i = 0; i < matrix.length; i++) {
+	if (i % 2 === 0) {
+		matrix[i].sort(oddSort);
+	}
+	else {
+		matrix[i].sort(evenSort);
+	}
+}
+
+let arr = matrix.flat();
+  return arr;
+}
+
+
